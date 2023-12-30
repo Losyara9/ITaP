b41febfe08bf291c549ddcb5d6f1ade8801b29f6
Losyara <bekh-s@mail.ru>
2023-12-30 15:16:58 +0300

    Create SalesRecord.java

---
+++ src/lab6/SalesRecord.java
@@ -0,0 +1,17 @@
+class SalesRecord {
+    private String productName;
+    private double price;
+
+    public SalesRecord(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
